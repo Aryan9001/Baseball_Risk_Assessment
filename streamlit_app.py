25c4c9a46cbb0215ed9e552cad4ac7f520662b7a
Aryan9001 <aryannandal545@gmail.com>
2025-05-11 17:22:58 +1000

    Fixed_streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # streamlit_app.py
-
-import streamlit as st
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import streamlit as st
 import uuid
 from main import process_video
 
